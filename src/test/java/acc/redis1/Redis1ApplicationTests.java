6562c8940e2fccd8f8bceaaf30a1e39f6e282245
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2023-02-12 11:23:59 +0900

    Simple Redis

---
+++ src/test/java/acc/redis1/Redis1ApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.redis1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redis1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
